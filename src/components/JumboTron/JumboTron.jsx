30c052fbe0cc3e8ca3bc74018314deda59084992
Ivan Fyodor <aamerson198@gmail.com>
2021-01-23 23:07:31 -0500

    pushing homepage url

--- src/components/JumboTron/JumboTron.jsx
+++ src/components/JumboTron/JumboTron.jsx
@@ -6,19 +6,6 @@ const JumboTron = () => {
       <h1 className="display-4 text-center">Welcome To Our User Directory!</h1>
 
       <hr className="my-4" />
-
-      
-      {/* <div className="create-form">
-        <div className="input-group mb-3">
-          <input
-            type="text"
-            className="form-control"
-            placeholder="Search Employee"
-            aria-label="Search Employee"
-              // onChange={handleSearchTerm}
-          ></input>
-        </div>
-      </div> */}
     </div>
   );
 };
